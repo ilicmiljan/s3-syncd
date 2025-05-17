a73a6d4a9a214b709bbde8640258503f797155cd
Miljan Ilic <hello@miljanilic.com>
2025-05-17 23:38:35 +0200

    feat(filesystem): add copy and delete mode

--- internal/config/types.go
+++ internal/config/types.go
@@ -16,12 +16,20 @@ type Buckets struct {
 	Buckets map[string]Bucket `yaml:"buckets"`
 }
 
+type Mode string
+
+const (
+	ModeRename Mode = "rename"
+	ModeCopy   Mode = "copy"
+)
+
 type Task struct {
 	ID       string  `yaml:"-"`
 	BucketID string  `yaml:"bucket"`
 	Cron     string  `yaml:"cron"`
 	Remote   string  `yaml:"remote"`
 	Local    string  `yaml:"local"`
+	Mode     Mode    `yaml:"mode"`
 	Bucket   *Bucket `yaml:"-"`
 }
 
--- internal/filesystem/local/local.go
+++ internal/filesystem/local/local.go
@@ -3,6 +3,7 @@ package local
 import (
 	"fmt"
 	"github.com/google/uuid"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -45,10 +46,50 @@ func GetLastModified(path string) (time.Time, error) {
 	return info.ModTime(), nil
 }
 
-func Replace(source, destination string) error {
+func Rename(source, destination string) error {
 	if err := os.MkdirAll(filepath.Dir(destination), 0755); err != nil {
 		return fmt.Errorf("failed to create destination directory: %w", err)
 	}
 
 	return os.Rename(source, destination)
+}
+
+func CopyAndDelete(source, destination string) error {
+	// Ensure the destination directory exists
+	if err := os.MkdirAll(filepath.Dir(destination), 0755); err != nil {
+		return fmt.Errorf("failed to create destination directory %s: %w", filepath.Dir(destination), err)
+	}
+
+	// Open the source file
+	sourceFile, err := os.Open(source)
+	if err != nil {
+		return fmt.Errorf("failed to open source file: %w", err)
+	}
+	defer sourceFile.Close()
+
+	// Create the destination file
+	destinationFile, err := os.Create(destination)
+	if err != nil {
+		return fmt.Errorf("failed to create destination file: %w", err)
+	}
+	defer func() {
+		_ = destinationFile.Close()
+	}()
+
+	// Copy data
+	if _, err := io.Copy(destinationFile, sourceFile); err != nil {
+		return fmt.Errorf("failed to copy file: %w", err)
+	}
+
+	// Flush to disk
+	if err := destinationFile.Sync(); err != nil {
+		return fmt.Errorf("failed to sync destination file: %w", err)
+	}
+
+	// Remove the source file
+	if err := os.Remove(source); err != nil {
+		return fmt.Errorf("failed to delete source file: %w", err)
+	}
+
+	return nil
 }
