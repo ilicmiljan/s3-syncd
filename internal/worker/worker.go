af284c3f8e41c3f6fc3db0c2a6d01903d15212cc
Miljan Ilic <hello@miljanilic.com>
2025-05-17 17:27:38 +0200

    feat(worker): add worker logic

---
+++ internal/worker/worker.go
@@ -0,0 +1,81 @@
+package worker
+
+import (
+	"context"
+	"fmt"
+	"go-sync-s3.miljanilic.com/internal/config"
+	"go-sync-s3.miljanilic.com/internal/filesystem/local"
+	"go-sync-s3.miljanilic.com/internal/filesystem/s3"
+	"log"
+)
+
+func Execute(ctx context.Context, t config.Task) error {
+	logPrefix := fmt.Sprintf("task[%s]:", t.ID)
+
+	if ctx.Err() != nil {
+		log.Printf("%s context cancelled before execution", logPrefix)
+		return ctx.Err()
+	}
+
+	log.Printf("%s starting execution...", logPrefix)
+
+	bucket := t.Bucket
+	if bucket == nil {
+		return fmt.Errorf("%s no bucket assigned", logPrefix)
+	}
+
+	bucket.WithS3Client()
+
+	tempFile, err := local.CreateTempFile("tmp/")
+	if err != nil {
+		return fmt.Errorf("%s failed to create temp file: %w", logPrefix, err)
+	}
+	defer tempFile.Cleanup()
+
+	source, err := s3.ParseS3Path(t.Remote)
+	if err != nil {
+		return fmt.Errorf("%s invalid remote path: %w", logPrefix, err)
+	}
+	log.Printf("%s resolved S3 path: %s", logPrefix, source)
+
+	if !ShouldDownload(ctx, t, bucket, source, logPrefix) {
+		log.Printf("%s skipping download: remote file %s has not been modified since local file %s", logPrefix, t.Remote, t.Local)
+		return nil
+	}
+
+	log.Printf("%s downloading from S3...", logPrefix)
+	err = s3.DownloadS3Object(ctx, bucket.S3Client, source, tempFile.File)
+	if err != nil {
+		return fmt.Errorf("%s download failed: %w", logPrefix, err)
+	}
+	log.Printf("%s download complete", logPrefix)
+
+	if err := tempFile.File.Close(); err != nil {
+		return fmt.Errorf("%s failed to close file: %w", logPrefix, err)
+	}
+
+	if err := local.Replace(tempFile.Path, t.Local); err != nil {
+		return fmt.Errorf("%s failed to rename temp file: %w", logPrefix, err)
+	}
+
+	log.Printf("%s moved %s to %s", logPrefix, tempFile.Path, t.Local)
+	return nil
+}
+
+func ShouldDownload(ctx context.Context, t config.Task, bucket *config.Bucket, source s3.BucketPath, logPrefix string) bool {
+	lastModifiedSource, err := s3.GetLastModified(ctx, bucket.S3Client, source)
+	if err != nil {
+		return true
+	}
+
+	log.Printf("%s file %s last modified: %s", logPrefix, t.Remote, lastModifiedSource)
+
+	lastModifiedDestination, err := local.GetLastModified(t.Local)
+	if err != nil {
+		return true
+	}
+
+	log.Printf("%s file %s last modified: %s", logPrefix, t.Local, lastModifiedDestination)
+
+	return lastModifiedSource.After(lastModifiedDestination)
+}
